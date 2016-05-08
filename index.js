7fbb99b056d00864b998800ddac497e9ce4105b8
khejing <khejing@hotmail.com>
2016-05-08 23:02:04 +0800

    设置日志级别为syslog日志级别

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var winston = require('winston');
 winston.add(require('winston-syslog').Syslog, {showLevel: false, json: true});
 winston.remove(winston.transports.Console);
+winston.setLevels(winston.config.syslog.levels);
 
 module.exports = winston;
